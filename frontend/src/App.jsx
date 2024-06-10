e1bbcc4c82bab5d6ef60f919539220a5eb67287f
Ardhilla Eka <86395605+dwlndr@users.noreply.github.com>
2024-06-10 13:36:56 +0700

    ++

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,33 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Dashboard from "./admin/pages/Dashboard";
 import Schedule from "./admin/pages/Schedule";
-import Sidebar from "./admin/components/Sidebar";
-import Navbar from "./admin/components/Navbar";
 import Movie from "./admin/pages/Movie";
 import Users from "./admin/pages/Users";
 import HomePage from "./user/pages/HomePages";
 import MoviePage from "./user/pages/MoviePage";
-import UserLayout from "./user/layout/UserLayout";
 import TheaterPage from "./user/pages/TheaterPages";
 import TicketPage from "./user/pages/TicketPages";
 import AccountPage from "./user/pages/AccountPages";
 
 function App() {
- return (
-  <>
-   <Router>
-    <Routes>
-     {/* Admin Routes */}
-     <Route path="/admin" element={<Dashboard />} />
-     <Route path="/admin/dashboard" element={<Dashboard />} />
-     <Route path="/admin/schedule" element={<Schedule />} />
-     <Route path="/admin/movie" element={<Movie />} />
-     <Route path="/admin/users" element={<Users />} />
-
-     {/* User Routes */}
-     <Route path="/" element={<UserLayout />}>
-      <Route index element={<HomePage />} />
-      <Route path="home" element={<HomePage />} />
-      <Route path="movie" element={<MoviePage />} />
-      <Route path="theater" element={<TheaterPage />} />
-      <Route path="ticket" element={<TicketPage />} />
-      <Route path="account" element={<AccountPage />} />
-     </Route>
-    </Routes>
-   </Router>
-  </>
- );
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/admin/dashboard" element={<Dashboard />} />
+          <Route path="/admin/schedule" element={<Schedule />} />
+          <Route path="/admin/movie" element={<Movie />} />
+          <Route path="/admin/users" element={<Users />} />
+          <Route path="/admin" element={<Dashboard />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movie" element={<MoviePage />} />
+          <Route path="/theater" element={<TheaterPage />} />
+          <Route path="/ticket" element={<TicketPage />} />
+          <Route path="/account" element={<AccountPage />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
--- frontend/src/user/pages/HomePages.jsx
+++ frontend/src/user/pages/HomePages.jsx
@@ -1,46 +1,52 @@
 import { Container, Col, Row } from "react-bootstrap";
 import HeroImage from "../assets/img/hero/hero1.png";
+import NavbarComponents from "../components/NavbarComponent";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../css/main.css";
 
 const HomePage = () => {
- return (
-  <div className="homepage">
-   <header className="w-100 min-vh-100 d-flex align-items-center">
-    <Container>
-     <Row className="header-box d-flex align-items-center">
-      <Col lg="6">
-       <h1 className="mb-4">
-        Get Your Own <br /> <span>Favourite Movie Ticket</span> <br /> Here!
-       </h1>
-       <p className="mb-4">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis
-        quis natus quisquam animi. Maiores, vel.
-       </p>
-       <button className="btn btn-light btn-lg rounded-1 me-2 mb-xs-0 mb-2">
-        Buy Ticket
-       </button>
-       <button className="btn btn-outline-light btn-lg rounded-1 mb-xs-0 mb-2">
-        Movies
-       </button>
-      </Col>
-      <Col lg="6" className="pt-lg-0 pt-5">
-       <img src={HeroImage} alt="" />
-      </Col>
-     </Row>
-    </Container>
-   </header>
-   <div className="movies w-100 min-vh-100">
-    <Container>
-     <Row>
-      <Col>
-       <h1 className="fw-bold ">Showing Now</h1>
-       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-      </Col>
-     </Row>
-     <Row></Row>
-    </Container>
-   </div>
-  </div>
- );
+  return (
+    <div className="homepage">
+      <NavbarComponents />
+      <header className="w-100 min-vh-100 d-flex align-items-center">
+        <Container>
+          <Row className="header-box d-flex align-items-center">
+            <Col lg="6">
+              <h1 className="mb-4">
+                Get Your Own <br /> <span>Favourite Movie Ticket</span> <br />{" "}
+                Here!
+              </h1>
+              <p className="mb-4">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Perferendis quis natus quisquam animi. Maiores, vel.
+              </p>
+              <button className="btn btn-light btn-lg rounded-1 me-2 mb-xs-0 mb-2">
+                Buy Ticket
+              </button>
+              <button className="btn btn-outline-light btn-lg rounded-1 mb-xs-0 mb-2">
+                Movies
+              </button>
+            </Col>
+            <Col lg="6" className="pt-lg-0 pt-5">
+              <img src={HeroImage} alt="" />
+            </Col>
+          </Row>
+        </Container>
+      </header>
+      <div className="movies w-100 min-vh-100">
+        <Container>
+          <Row>
+            <Col>
+              <h1 className="fw-bold ">Showing Now</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </Col>
+          </Row>
+          <Row></Row>
+        </Container>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
