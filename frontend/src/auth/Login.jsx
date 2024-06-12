0c01f9e679dfcfd07bb7f4353c176423e84bc400
Nu <sitiainunrodhiyah10@gmail.com>
2024-06-12 12:57:23 +0700

    perbaikan css login

--- frontend/src/auth/Login.jsx
+++ frontend/src/auth/Login.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { UserContext } from '../UserContext';
-// import './css/loginregister.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const Login = () => {
--- frontend/src/auth/Register.jsx
+++ frontend/src/auth/Register.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-// import './css/loginregister.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const Register = () => {
