9ced599b69cb01e5466227b4386bc14f66b05a59
yumna <hayayumnaa03@gmail.com>
2024-06-14 12:53:02 +0700

    benerin button

---
+++ frontend/src/user/data/index.js
@@ -0,0 +1,173 @@
+import poster1 from "../assets/img/movie/movie-1.jpeg";
+import poster2 from "../assets/img/movie/movie-2.jpeg";
+import poster3 from "../assets/img/movie/movie-3.jpeg";
+import poster4 from "../assets/img/movie/movie-4.jpeg";
+import poster5 from "../assets/img/movie/movie-5.jpeg";
+import poster6 from "../assets/img/movie/movie-6.jpeg";
+import poster7 from "../assets/img/movie/movie-7.jpeg";
+import poster8 from "../assets/img/movie/movie-8.jpeg";
+
+import promo1 from "../assets/img/promo/promo1.png";
+import promo2 from "../assets/img/promo/promo2.png";
+import promo3 from "../assets/img/promo/promo3.png";
+import promo4 from "../assets/img/promo/promo4.png";
+
+export const promos = [
+ { id: 1, image: promo1 },
+ { id: 2, image: promo2 },
+ { id: 3, image: promo3 },
+ { id: 4, image: promo4 },
+];
+
+export const posterMovies = [
+ {
+  id: 1,
+  image: poster1,
+  title: "Badarawuhi di Desa Penari",
+  genre: "Horror",
+  age: "13+",
+  duration: "112 minutes",
+  director: "Kimo Stamboel",
+  writer: "Laila Nurazizah",
+  price: "Rp 45000,-",
+  cast: "Aulia Sarah, Maudy Effrosina, Jourdy Pranata",
+  synopsis:
+   "Mila, bersama dengan sepupunya, Yuda, dan sahabat mereka, Arya dan Jito, pergi menuju ke sebuah desa terpencil di tengah hutan yang dikenal sebagai Desa Penari. Mila percaya bahwa jika dia mengembalikan sebuah gelang antik milik Badarawuhi, sesosok entitas gaib yang menguasai desa tersebut, dapat menyembuhkan penyakit ibunya. Namun, setelah Mila berhasil mengembalikan gelang tersebut, keadaan justru malah semakin memburuk.",
+ },
+ {
+  id: 2,
+  image: poster2,
+  title: "DILAN 1983 WO AI NI",
+  genre: "Drama",
+  duration: "1h 56m",
+  director: "Fajar Bustomi",
+  cast: "Muhammad Adhiyat",
+  synopsis:
+   "Tahun 1983, setelah satu setengah tahun tinggal di Timor Timur, Dilan kembali ke Bandung. Dilan pun kembali bertemu dengan teman-teman lamanya di SD tempat dulu dia sekolah. Tapi, ternyata ada murid baru pindahan dari Semarang, namanya Mei Lien, gadis keturunan Tionghoa.",
+ },
+ {
+  id: 3,
+  image: poster3,
+  title: "DILAN 1983 WO AI NI",
+  genre: "Drama",
+  duration: "1h 56m",
+  director: "Fajar Bustomi",
+  cast: "Muhammad Adhiyat",
+  synopsis:
+   "Tahun 1983, setelah satu setengah tahun tinggal di Timor Timur, Dilan kembali ke Bandung. Dilan pun kembali bertemu dengan teman-teman lamanya di SD tempat dulu dia sekolah. Tapi, ternyata ada murid baru pindahan dari Semarang, namanya Mei Lien, gadis keturunan Tionghoa.",
+ },
+ {
+  id: 4,
+  image: poster4,
+  title: "DILAN 1983 WO AI NI",
+  genre: "Drama",
+  duration: "1h 56m",
+  director: "Fajar Bustomi",
+  cast: "Muhammad Adhiyat",
+  synopsis:
+   "Tahun 1983, setelah satu setengah tahun tinggal di Timor Timur, Dilan kembali ke Bandung. Dilan pun kembali bertemu dengan teman-teman lamanya di SD tempat dulu dia sekolah. Tapi, ternyata ada murid baru pindahan dari Semarang, namanya Mei Lien, gadis keturunan Tionghoa.",
+ },
+ {
+  id: 5,
+  image: poster5,
+  title: "DILAN 1983 WO AI NI",
+  genre: "Drama",
+  duration: "1h 56m",
+  director: "Fajar Bustomi",
+  cast: "Muhammad Adhiyat",
+  synopsis:
+   "Tahun 1983, setelah satu setengah tahun tinggal di Timor Timur, Dilan kembali ke Bandung. Dilan pun kembali bertemu dengan teman-teman lamanya di SD tempat dulu dia sekolah. Tapi, ternyata ada murid baru pindahan dari Semarang, namanya Mei Lien, gadis keturunan Tionghoa.",
+ },
+ {
+  id: 6,
+  image: poster6,
+  title: "DILAN 1983 WO AI NI",
+  genre: "Drama",
+  duration: "1h 56m",
+  director: "Fajar Bustomi",
+  cast: "Muhammad Adhiyat",
+  synopsis:
+   "Tahun 1983, setelah satu setengah tahun tinggal di Timor Timur, Dilan kembali ke Bandung. Dilan pun kembali bertemu dengan teman-teman lamanya di SD tempat dulu dia sekolah. Tapi, ternyata ada murid baru pindahan dari Semarang, namanya Mei Lien, gadis keturunan Tionghoa.",
+ },
+ {
+  id: 7,
+  image: poster7,
+  title: "DILAN 1983 WO AI NI",
+  genre: "Drama",
+  duration: "1h 56m",
+  director: "Fajar Bustomi",
+  cast: "Muhammad Adhiyat",
+  synopsis:
+   "Tahun 1983, setelah satu setengah tahun tinggal di Timor Timur, Dilan kembali ke Bandung. Dilan pun kembali bertemu dengan teman-teman lamanya di SD tempat dulu dia sekolah. Tapi, ternyata ada murid baru pindahan dari Semarang, namanya Mei Lien, gadis keturunan Tionghoa.",
+ },
+ {
+  id: 8,
+  image: poster8,
+  title: "DILAN 1983 WO AI NI",
+  genre: "Drama",
+  duration: "1h 56m",
+  director: "Fajar Bustomi",
+  cast: "Muhammad Adhiyat",
+  synopsis:
+   "Tahun 1983, setelah satu setengah tahun tinggal di Timor Timur, Dilan kembali ke Bandung. Dilan pun kembali bertemu dengan teman-teman lamanya di SD tempat dulu dia sekolah. Tapi, ternyata ada murid baru pindahan dari Semarang, namanya Mei Lien, gadis keturunan Tionghoa.",
+ },
+];
+
+export const faq = [
+ {
+  id: 1,
+  eventKey: 0,
+  title: "What is FlickBox?",
+  desc:
+   "FlickBox is an online platform that allows you to book movie tickets easily and conveniently from your computer or mobile device.",
+ },
+ {
+  id: 2,
+  eventKey: 1,
+  title: "How do I create an account on FlickBox?",
+  desc:
+   "To create an account, click on the 'Sign Up' button on the top right corner of the homepage. Fill in the required details and follow the instructions to complete the registration process.",
+ },
+ {
+  id: 3,
+  eventKey: 2,
+  title: "How do I know if my booking was successful?",
+  desc:
+   "After completing the payment, you can check the status of your booking under 'My Ticket' in your FlickBox account.",
+ },
+ {
+  id: 4,
+  eventKey: 3,
+  title: "How do I book a movie ticket on FlickBox?",
+  desc:
+   "1. Log in to your FlickBox account. 2. Browse through the list ofmovies or use the search bar to find your desired movie. 3. Select the movie, showtime, and seat(s). 4. Proceed to checkout and make the payment. 5. You will receive a confirmation email and your e-ticket.",
+ },
+ {
+  id: 5,
+  eventKey: 4,
+  title: "Can I choose my seat while booking tickets?",
+  desc:
+   "Yes, FlickBox allows you to choose your preferred seats while booking tickets. Simply click on the available seats shown in the seating layout during the booking process.",
+ },
+ {
+  id: 6,
+  eventKey: 5,
+  title: "Can I use a promo code or discount while booking tickets?",
+  desc:
+   "Yes, FlickBox offers promo codes and discounts from time to time. You can enter your promo code at the checkout to avail of the discount.",
+ },
+ {
+  id: 7,
+  eventKey: 6,
+  title: "What payment methods are accepted on FlickBox?",
+  desc:
+   "FlickBox accepts various payment methods including credit/debit cards digital wallets, and net banking.",
+ },
+ {
+  id: 8,
+  eventKey: 7,
+  title: "Is it safe to make payments on FlickBox?",
+  desc:
+   "Yes, FlickBox uses secure payment gateways and advanced encryption technologies to ensure that your transactions are safe and secure.",
+ },
+];
