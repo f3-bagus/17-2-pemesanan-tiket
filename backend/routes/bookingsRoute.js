dadd228a9e4df644d560c9f960231b8de9c0d311
fairuz akhdan <fairuzakhdan01@gmail.com>
2024-06-18 23:50:15 +0700

    update seat booking

--- backend/routes/bookingsRoute.js
+++ backend/routes/bookingsRoute.js
@@ -2,11 +2,10 @@ const express = require("express");
 const router = express.Router();
 const bookingsController = require("../controllers/bookingsController");
 const { verifyToken } = require('../middlewares/verifyToken');
-const { clearTempSeats } = require('../middlewares/saveSeat')
 
 router
   .route("/:id/order")
-  .post(verifyToken ,clearTempSeats,bookingsController.createBookingOrder)
+  .post(verifyToken ,bookingsController.createBookingOrder)
   .get(verifyToken ,bookingsController.getBookingOrderByUser);
 
 module.exports = router;
