3bd2fc1b3a07900f2cc915c95b01e96e625a5b93
fairuz akhdan <fairuzakhdan01@gmail.com>
2024-06-28 14:12:31 +0700

    update

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,13 @@
 require("dotenv").config();
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 const path = require("path");
 const morgan = require("morgan");
 const cors = require("cors");
+
+const app = express();
 const port = process.env.PORT || 5000;
 const usersRouter = require("./routes/usersRoute");
 const authRouter = require("./routes/auth");
@@ -20,7 +21,7 @@ app.use(
     origin: "http://localhost:5750",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true, 
+    credentials: true,
   })
 );
 app.use(morgan("dev"));
@@ -31,12 +32,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "./public")));
 
 mongoose
-  .connect(process.env.DB_NAME)
+  .connect(process.env.DB_NAME, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Database connected");
   })
   .catch((err) => {
-    console.log("Database is not connected", err.message);
+    console.error("Database connection error:", err.message);
   });
 
 app.use("/api/films", filmsRouter);
@@ -50,14 +51,14 @@ app.use((req, res) => {
   res.status(404).json({
     message: "Page Not Found",
   });
-  // next();
 });
 
-app.use((err, req, res) => {
+app.use((err, req, res, next) => { // Menambahkan parameter next
   console.error(err.stack);
   res.status(500).json({ error: "Internal Server Error" });
 });
 
 app.listen(port, () => {
-  console.log(`server is running`);
+  console.log(`Server is running on port ${port}`);
 });
+
